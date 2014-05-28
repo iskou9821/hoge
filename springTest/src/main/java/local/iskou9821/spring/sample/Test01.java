bed2134fd0fa3552b9b6e32d6746e9a98108dd1f
iskou9821 <kou.ishikawa@gmail.com>
2014-05-28 18:54:12 +0900

    Spring経由でデータソースを取得するテスト

--- springTest/src/main/java/local/iskou9821/spring/sample/Test01.java
+++ springTest/src/main/java/local/iskou9821/spring/sample/Test01.java
@@ -1,9 +1,18 @@
 package local.iskou9821.spring.sample;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
 import local.iskou9821.spring.sample.bean.HogeBean;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 
 
 public class Test01 {
@@ -12,5 +21,20 @@ public class Test01 {
 				new ClassPathXmlApplicationContext("applicationContext.xml");
 		HogeBean hoge = (HogeBean)ctx.getBean("hoge");
 		System.out.println(hoge.getMsg());
+		
+		DataSource ds = (DataSource)ctx.getBean("dataSource");
+		try (Connection conn = DataSourceUtils.getConnection(ds)) {
+			try (Statement st = conn.createStatement()) {
+				st.execute("create table hoge(id integer, name varchar(100))");
+				st.execute("insert into hoge(id,name) values(10, 'aaaaa') ");
+				try (ResultSet rs = st.executeQuery("select * from hoge")) {
+					while (rs.next()) {
+						System.out.println(rs.getInt(1) + " / " + rs.getString(2));
+					}
+				}
+			}
+		} catch (CannotGetJdbcConnectionException|SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
